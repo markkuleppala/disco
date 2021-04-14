ac8ceb753fc7115aaab8c86f683bdd3a3883eb0d
Markku Leppälä <38980504+markkuleppala@users.noreply.github.com>
2021-04-14 16:32:09 +0300

    Update test.sh

--- deployments/test.sh
+++ deployments/test.sh
@@ -5,13 +5,13 @@ RW="read  write  randread  randwrite"
 
 for NUM_JOBS in 1 2 3
 do
-	echo Jobs num: $NUM_JOBS		
+    echo -e 'Jobs num: $NUM_JOBS\n'		
     for BS in 512 1024 2048 4096 8192 16384 32768 65536
     do
-    	echo Block size: $BS
+    	echo -e 'Block size: $BS\n'
         for FIO_RW in $RW
         do
-        	echo IO pattern: $FIO_RW
+            echo -e 'IO pattern: $FIO_RW\n'
             for i in 1 2 3
             do
                 RUNTIME=30 FILESIZE=1G SIZE=1G NUM_JOBS=$NUM_JOBS FIO_RW=$FIO_RW \
