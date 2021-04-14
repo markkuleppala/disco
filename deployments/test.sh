2bfb92a8e4eeefb2405c6e28e98b07fb32e73590
Markku Leppälä <38980504+markkuleppala@users.noreply.github.com>
2021-04-14 11:46:54 +0300

    Update test.sh

--- deployments/test.sh
+++ deployments/test.sh
@@ -5,22 +5,25 @@ RW="read  write  randread  randwrite"
 
 for NUM_JOBS in 1 2 3
 do
-        for BS in 512 1024 2048 4096 8192 16384 32768 65536
+	echo Jobs num: $NUM_JOBS		
+    for BS in 512 1024 2048 4096 8192 16384 32768 65536
+    do
+    	echo Block size: $BS
+        for FIO_RW in $RW
         do
-                for FIO_RW in $RW
-                do
-                        for i in {1..3..1}
-                        do
-                                RUNTIME=30 FILESIZE=1G SIZE=1G NUM_JOBS=$NUM_JOBS FIO_RW=$FIO_RW \
-                                        fio fio_jobfile.fio \
-                                        --directory=/emptydir \
-                                        --output-format=json+ \
-                                        --blocksize=$BS \
-                                        --output=BS$BS-J$NUM_JOBS-$FIO_RW-${i}.json
-                                rm -rf emptydir/*.dat
-                        done
-                done
+        	echo IO pattern: $FIO_RW
+            for i in 1 2 3
+            do
+                RUNTIME=30 FILESIZE=1G SIZE=1G NUM_JOBS=$NUM_JOBS FIO_RW=$FIO_RW \
+                    fio fio_jobfile.fio \
+                    --directory=/emptydir \
+                    --output-format=json+ \
+                    --blocksize=$BS \
+                    --output=BS$BS-J$NUM_JOBS-$FIO_RW-$i.json
+                rm -rf emptydir/*.dat
+            done
         done
+    done
 done
 
 mkdir Results
